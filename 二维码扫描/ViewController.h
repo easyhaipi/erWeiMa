bb1fd9836a8ad9e8a1af325b5135b0a43d75038d
easyhaipi <easy_qingsong@163.com>
2016-07-25 19:43:45 +0800

    Initial Commit

---
+++ 二维码扫描/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  二维码扫描
+//
+//  Created by taobaichi on 16/7/25.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 二维码扫描/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  二维码扫描
+//
+//  Created by taobaichi on 16/7/25.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
