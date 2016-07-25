02cfc7e7c479d246f9dcb08c580aad8c7d3cf412
easyhaipi <easy_qingsong@163.com>
2016-07-25 19:49:16 +0800

    二维码扫描

--- 二维码扫描/ViewController.m
+++ 二维码扫描/ViewController.m
@@ -7,16 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate>
 
-@interface ViewController ()
-
+@property(nonatomic,strong)AVCaptureSession *session;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //获取摄像设备
+    AVCaptureDevice * device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    //创建输入流
+    AVCaptureDeviceInput * input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
+    
+    //创建输出流
+    AVCaptureMetadataOutput * output = [[AVCaptureMetadataOutput alloc]init];
+    
+    //设置代理 在主线程里刷新
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    //初始化链接对象
+    self.session = [[AVCaptureSession alloc]init];
+    //高质量采集率
+    [self.session setSessionPreset:AVCaptureSessionPresetHigh];
+    
+    [self.session addInput:input];
+    [self.session addOutput:output];
+    
+    //设置扫码支持的编码格式(如下设置条形码和二维码兼容)
+    output.metadataObjectTypes=@[AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
+    
+    AVCaptureVideoPreviewLayer * layer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    layer.videoGravity=AVLayerVideoGravityResizeAspectFill;
+    //扫描框的位置和大小
+    layer.frame = CGRectMake(60, 100, 200, 200);
+    [self.view.layer insertSublayer:layer atIndex:0];
+    
+    //开始捕获
+    [self.session startRunning];
+
 }
 
 - (void)didReceiveMemoryWarning {
